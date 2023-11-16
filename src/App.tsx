e2ab9ea89d5de6ebc4702041b5d930ec2953b789
Adlee <adlee@qburst.com>
2023-11-16 16:21:44 +0530

    webworker done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useMemo, useState } from "react";
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    </>
-  )
+  const [color, setColor] = useState(0);
+  // const worker: Worker = useMemo(
+  //   () => new Worker(new URL("src/worker.js", import.meta.url)),
+  //   []
+  // );
+  const handleevent = () => {
+    const webWorker = new Worker("worker.js");
+    webWorker.onmessage = function (e) {
+      console.log("Msg recieved", e.data);
+      webWorker.terminate();
+    };
+    webWorker.postMessage([5, 10]);
+  };
+  return <div onClick={handleevent}>Hello</div>;
 }
 
-export default App
+export default App;
